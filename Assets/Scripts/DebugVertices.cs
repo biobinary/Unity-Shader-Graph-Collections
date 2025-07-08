7fc3f7a382a561a99486c0c8c42ebac01c0c2843
biobinary <mmcury2019@gmail.com>
2025-07-08 16:54:16 +0700

    Add DebugVertices script for vertex visualization
    Introduced DebugVertices MonoBehaviour to display mesh vertex positions as labels in the Unity editor using Gizmos. Added associated meta files for Unity asset management.

---
+++ Assets/Scripts/DebugVertices.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class DebugVertices : MonoBehaviour {
+
+	Mesh mesh;
+	Vector3[] vertices;
+
+	void OnDrawGizmos() {
+
+		if (vertices == null) {
+			mesh = GetComponent<MeshFilter>().sharedMesh;
+			vertices = mesh.vertices;
+		}
+		
+		foreach (Vector3 v in vertices)
+            UnityEditor.Handles.Label(transform.position + v, "v: " + v.ToString());
+
+	}
+}
